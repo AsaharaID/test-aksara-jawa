35bb2c3bfc7c0891640a278662802a4abcc571b5
Syah Reza <muhamadsyahr45@gmail.com>
2022-02-23 11:34:38 +0700

    commit

--- Components/SideBar/footer.tsx
+++ Components/SideBar/footer.tsx
@@ -4,12 +4,7 @@ export default function Footer() {
 	return (
 		<div className="sidebar-footer pt-73 pe-30">
 			<div className="footer-card">
-				<div className="d-flex justify-content-between mb-20">
-					<p className="fw-medium color-palette-1">
-						{/* Top Up &<br />
-						Be The Winner */}
-					</p>
-				</div>
+				<div className="d-flex justify-content-between mb-20"></div>
 			</div>
 		</div>
 	);
